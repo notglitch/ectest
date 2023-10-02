35bdc4af1bd8d89273801b7861b9a2eb7945cf4f
ariadna <ariadnamborges@gmail.com>
2023-10-02 15:00:20 -0400

    [ADD] Description & Details of SM for Website Page

--- models/res_partner.py
+++ models/res_partner.py
@@ -18,9 +18,19 @@ class Partner(models.Model):
             else:
                 customer.image = False
 
+    facebook_followers = fields.Float(string="facebook followers", store=True)
+    facebook_followers_sig = fields.Selection(
+        [("few", "<k"), ("k", "K"), ("m", "M"), ("mm", "MM")], default="few",
+        string="measure", store=True)
     facebook = fields.Char(string="Facebook", store=True)
+    linkedin_followers = fields.Float(string="linkedin followers", store=True)
+    linkedin_followers_sig = fields.Selection(
+        [("few", "<k"), ("k", "K"), ("m", "M"), ("mm", "MM")], default="few",
+        string="measure", store=True)
     linkedin = fields.Char(string="Linkedin", store=True)
     twitter = fields.Char(string="Twitter", store=True)
+    twitter_followers = fields.Float('twitter followers')
+    twitter_followers_sig = fields.Selection(
+        [("few", "<k"), ("k", "K"), ("m", "M"), ("mm", "MM")], default="few")
+    website_brief_info = fields.Text(string="Website Brief info")
     image = fields.Image(string="Profile Completed", compute='_compute_image')
-
-
